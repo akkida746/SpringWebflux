737006743251e168d040cf0ec5daf74d23888987
Akash <akkida746@gmail.com>
2022-03-08 20:05:36 +0530

    websocket

---
+++ src/main/java/com/example/springwebflux/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.example.springwebflux.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class WebSocketController {
+
+    @GetMapping("/websocket")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name");
+        return "ws-broadcast";
+    }
+}
---
+++ src/main/java/com/example/springwebflux/handler/MyWebSocketHandler.java
@@ -0,0 +1,42 @@
+package com.example.springwebflux.handler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+@Slf4j
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        sessions.forEach(webSocketSession -> {
+            try {
+                webSocketSession.sendMessage(message);
+            } catch (IOException e) {
+                log.error("Error occurred.", e);
+            }
+        });
+    }
+}
