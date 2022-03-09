317fbc59b45bc895915310d6feca9a2e76c6d521
Akash <akkida746@gmail.com>
2022-03-09 07:52:39 +0530

    refactor

---
+++ src/main/java/com/example/springwebflux/controller/ConsumerController.java
@@ -0,0 +1,14 @@
+package com.example.springwebflux.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.net.http.HttpClient;
+
+@RestController
+public class ConsumerController {
+
+    public void consumeUserDetails(){
+
+    }
+}
